c11ce5b4ec6a370cb583366138f19750ed48b91b
tsukuyomip <ab0312uo@gmail.com>
2014-09-19 03:22:48 +0900

    クラス Stroke を追加
    logPaint等で一部プログラム修正

---
+++ code/Stroke.py
@@ -0,0 +1,100 @@
+# coding: utf-8
+import sys
+
+class Stroke(object):
+    def __init__(self, rawData = None):
+        """
+        rawData = [(time, x, y)...]
+        data = [(time, x, y)...]
+        rawDataのキャプチャ点で省かれた部分を補完．（data要素数 = totTime になる）
+        data内要素のx, yは [0.0 : 1.0] で正規化される．
+        """
+
+        if (rawData is None):
+            rawData = [(0, 0, 0)]
+        self.rawData = rawData
+        self.nRawData = len(rawData)
+        if(self.nRawData == 0):
+            self.outputError("class Stroke.__init__", 
+                             "nRawData is 0")
+
+        if(len(rawData[0]) != 3):
+            self.outputError("class Stroke.__init__", 
+                             "elem of data len isn't 3 (t, x, y)")
+
+        self.totTime = rawData[-1][0]
+
+        (minX, maxX, minY, maxY) = self.getMinMax(rawData)
+        self.minX = minX
+        self.maxX = maxX
+        self.minY = minY
+        self.maxY = maxY
+
+        data = self.normalizeRawData(rawData)
+
+    def getMinMax(self, rawData):
+        minX = rawData[0][1]
+        maxX = rawData[0][1]
+        minY = rawData[0][2]
+        maxY = rawData[0][2]
+        for i in range(1, self.nRawData):
+            if(minX > rawData[i][1]):
+                minX = rawData[i][1]
+            if(maxX < rawData[i][1]):
+                maxX = rawData[i][1]
+            if(minY > rawData[i][2]):
+                minY = rawData[i][2]
+            if(maxY < rawData[i][2]):
+                maxY = rawData[i][2]
+
+        return (minX, maxX, minY, maxY)
+
+
+    def normalizeRawData(self, rawData):
+        """
+        与えられたrawDataのX, Yを[0.0 : 1.0]に正規化．
+        各t間を補完し，dataとして補完結果を格納
+        """
+        xSize = self.maxX - self.minX
+        if(xSize <= 0):
+            self.outputError("class Stroke.normalizeRawData", 
+                             "xSize(%d) <= 0" % (xSize))
+
+        ySize = self.maxY - self.minY
+        if(ySize <= 0):
+            self.outputError("class Stroke.normalizeRawData", 
+                             "ySize(%d) <= 0" % (ySize))
+
+        data = []
+        t = 0
+
+        nmlX = float(rawData[0][1] - self.minX)/float(xSize)
+        nmlY = float(rawData[0][2] - self.minY)/float(ySize)
+        data.append((t, nmlX, nmlY))
+        prevIndex = 0
+        t += 1
+
+        while(prevIndex < self.nRawData - 1):
+            if(rawData[prevIndex + 1][0] > t):
+                nmlX = data[-1][1]
+                nmlY = data[-1][2]
+            else:
+                nmlX = float(rawData[prevIndex + 1][1] - self.minX)/float(xSize)
+                nmlY = float(rawData[prevIndex + 1][2] - self.minY)/float(ySize)
+                prevIndex += 1
+
+            data.append((t, nmlX, nmlY))
+            t += 1
+
+        self.data = data
+
+    def outputError(self, errPoint, mes):
+        print >> sys.stderr, "Error(%s): %s" % (str(errPoint), str(mes))
+
+if __name__ == "__main__":
+    import cPickle
+    inputFilename = "testLog.pkl"
+    outputFilename = "testStroke.pkl"
+    log = cPickle.load(open(inputFilename, "rb"))
+    s = Stroke(log)
+    cPickle.dump(s, open(outputFilename, "wb"))
--- code/logPaint.py
+++ code/logPaint.py
@@ -1,5 +1,6 @@
 import wx
 import time
+import cPickle as cpkl
 
 class DrawPanel(wx.ScrolledWindow):
         def __init__(self, parent):
@@ -13,49 +14,68 @@ class DrawPanel(wx.ScrolledWindow):
                 self.Bind(wx.EVT_LEFT_DOWN, self.OnMouseLeftDown)
                 self.Bind(wx.EVT_MOTION, self.OnMouseMotion)
                 self.Bind(wx.EVT_LEFT_UP, self.OnMouseLeftUp)
+                self.Bind(wx.EVT_RIGHT_DOWN, self.OnMouseRightDown)
 
                 self.dragFlg = False
-
+                self.logData = None
 
         def OnMouseLeftDown(self, event):
+                # initialize canvas
                 dc = wx.BufferedDC(None, self.buffer)
                 #dc.SetBackground(wx.Brush(self.GetBackgroundColour()))
                 #self.SetBackgroundColour("white")
                 dc.SetPen(wx.Pen("WHITE", 2))
-                dc.DrawRectangle(0, 0, 500, 500)
+                dc.DrawRectangle(0, 0, 512, 512)
                 dc.Clear()
 
+                # get position & time
                 pos = event.GetPosition()
                 self.px = pos[0]
                 self.py = pos[1]
                 self.dragFlg = True
-
                 self.startTime = int(time.time()*1000)
+
+                # initialize log
+                self.logData = []
+                self.logData.append((0, self.px, self.py))
                 self.outputLog("%d\t%d\t%d" % (0, self.px, self.py))
 
 
         def OnMouseMotion(self, event):
                 if self.dragFlg == True:
+                        # get position & time
                         pos = event.GetPosition()
                         nx = pos[0]
                         ny = pos[1]
                         t = int(time.time()*1000) - self.startTime
                         t = int((t/10) + 1)
+
+                        # add log
+                        self.logData.append((t, nx, ny))
                         self.outputLog("%d\t%d\t%d" % (t, nx, ny))
 
-
+                        # draw line
                         dc = wx.BufferedDC(None, self.buffer)
                         dc.SetPen(wx.Pen("BLACK", 2))
                         dc.DrawLine(self.px, self.py, nx, ny)
 
+                        # update previous x, y
                         self.px = nx
                         self.py = ny
 
+                        # show
                         self.Refresh()
 
         def OnMouseLeftUp(self, event):
                 self.dragFlg = False
                 self.outputLog()
+
+        def OnMouseRightDown(self, event):
+                # if drawn, dump logData
+                if self.logData != None:
+                        filename = "testLog.pkl"
+                        cpkl.dump(self.logData, open(filename, "wb"))
+                        print "output logfile:", filename
 
         def outputLog(self, str = ""):
                 print str
@@ -64,6 +84,8 @@ class DrawPanel(wx.ScrolledWindow):
                 dc = wx.BufferedDC(None, self.buffer)
                 dc.SetBackground(wx.Brush(self.GetBackgroundColour()))
                 dc.Clear()
+                dc.SetPen(wx.Pen("#FF9999", 2))
+                dc.DrawRectangle(0, 0, 512, 512)
                 #dc.SetPen(wx.Pen("BLACK", 1))
                 #dc.DrawCircle(100, 100, 10)
                 #dc.DrawCircle(2000, 100, 10)
